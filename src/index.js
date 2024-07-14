c485c2b05b4998f3f7ac499bd265b391246744bb
ah1623 <ah1623801@gmail.com>
2024-07-14 01:29:31 -0700

    ssde

--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home';
+import Html from './pages/html';
+import Javascript from './pages/javascript';
+import Css from './pages/css';
+const router = createBrowserRouter([
+  {
+    path: "/html",
+    element: <Html />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+  {
+    path: "/css",
+    element: <Css />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
